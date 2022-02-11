e332a2e7b2594ab85f55a442f822d600f5d474b4
taadis <i@taadis.com>
2022-02-12 01:15:38 +0800

    redis-cli 0.091 (new formula)
    * redis-cli 0.091 (new formula)
    * fix name
    
    Closes #1.
    
    Signed-off-by: taadis <16029681+taadis@users.noreply.github.com>

---
+++ Formula/redis-cli.rb
@@ -0,0 +1,18 @@
+class RedisCli < Formula
+  desc "Install the redis-cli only"
+  homepage "https://github.com/taadis/homebrew-tap"
+  url "https://download.redis.io/releases/redis-0.091.tar.gz"
+  sha256 "d1958a9b4b1e23c6b9eb778b8fdd06381ee21bdd7bb124c8b87abb933ee9d0f1"
+  license "MIT"
+
+  depends_on "make" => :build
+
+  def install
+    system "make", "redis-cli"
+    bin.install "./redis-cli"
+  end
+
+  test do
+    system "true"
+  end
+end
