7eb365f63caf098a13210ce30e358beef439110e
taadis <16029681+taadis@users.noreply.github.com>
2022-03-18 05:21:30 +0000

    redis-cli: update 1.1.92-beta bottle.

--- Formula/redis-cli.rb
+++ Formula/redis-cli.rb
@@ -6,10 +6,10 @@ class RedisCli < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/taadis/homebrew-tap/releases/download/redis-cli-1.1.91-beta"
-    sha256 cellar: :any_skip_relocation, big_sur:      "d61a31d93073791861cd8e812c4370721509e72c1d6f65e18ab775dd744cb8eb"
-    sha256 cellar: :any_skip_relocation, catalina:     "c063110591d04bd2fd05d7d8e91d8cf10798f0a42a411f38e01cb8da8de974b0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "3eeae0801b4620006d64851c3c4cc7681a4912e7d590bba67b31479afcbab496"
+    root_url "https://github.com/taadis/homebrew-tap/releases/download/redis-cli-1.1.92-beta"
+    sha256 cellar: :any_skip_relocation, big_sur:      "225c87ac7c12cc79a7773616bb9a3eb7bfd917452a045706c587da7fb93742a0"
+    sha256 cellar: :any_skip_relocation, catalina:     "863296f9c9d285cce5c4003527d3c3792ba77eb0b09192525c2cfc55b0152a99"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "1b2690e6e16d6a69ecb728246688a5cd6beb6c5ffac14eac369f5b57dc66524f"
   end
 
   depends_on "make" => :build
